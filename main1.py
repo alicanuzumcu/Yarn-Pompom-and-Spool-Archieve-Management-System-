4829b657967067093225980dd331f6a99f02d14e
alicanuzumcu <salican341@gmail.com>
2025-01-12 00:13:47 +0300

    README.md and others added

--- main1.py
+++ main1.py
@@ -8,11 +8,11 @@ from datetime import datetime
 class CRUDApp(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Ponpon Raf Sistemi")
+        self.setWindowTitle("Pompom Archieve Management System")
         self.setGeometry(100, 100, 900, 600)
         self.setStyleSheet("background-color: white;")
 
-        # "+" butonu
+        # "Insert" button
         self.add_record_button = QtWidgets.QPushButton()
         self.add_record_button.setIcon(QtGui.QIcon("photos/insert.png"))
         self.add_record_button.setIconSize(QtCore.QSize(45, 45))
@@ -35,12 +35,12 @@ class CRUDApp(QtWidgets.QWidget):
             font-family: Arial;
         """)
 
-        # "Güncelle" butonu
+        # "Update" butonu
         self.update_button = QtWidgets.QPushButton()
-        self.update_button.setIcon(QtGui.QIcon("update.png"))
+        self.update_button.setIcon(QtGui.QIcon("photos/update.png"))
         self.update_button.setIconSize(QtCore.QSize(45, 45))
         self.update_button.clicked.connect(self.open_update_window)
-        self.update_button.setEnabled(False)  # Başlangıçta devre dışı
+        self.update_button.setEnabled(False)  
         self.update_button.setFixedSize(60, 60)
         self.update_button.setToolTip("Update record")
         self.update_button.setStyleSheet("""
@@ -59,12 +59,12 @@ class CRUDApp(QtWidgets.QWidget):
             font-family: Arial;
         """)
 
-        # "Sil" butonu
+        # "Delete" button
         self.delete_button = QtWidgets.QPushButton()
-        self.delete_button.setIcon(QtGui.QIcon("delete2.png"))
+        self.delete_button.setIcon(QtGui.QIcon("photos/delete2.png"))
         self.delete_button.setIconSize(QtCore.QSize(45, 45))
         self.delete_button.clicked.connect(self.delete_record)
-        self.delete_button.setEnabled(False)  # Başlangıçta devre dışı
+        self.delete_button.setEnabled(False) 
         self.delete_button.setFixedSize(60, 60)
         self.delete_button.setToolTip("Delete record")
         self.delete_button.setStyleSheet("""
@@ -106,7 +106,7 @@ class CRUDApp(QtWidgets.QWidget):
         # Apply the bold font to the header
         self.record_table.horizontalHeader().setFont(header_font)
 
-        # Satır seçimi için sinyal
+        # Row selection signal
         self.record_table.itemSelectionChanged.connect(self.enable_buttons)
 
         # Layout
@@ -114,14 +114,14 @@ class CRUDApp(QtWidgets.QWidget):
         Hlayout = QtWidgets.QHBoxLayout()
         Hlayout.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeft)
         Hlayout.addWidget(self.add_record_button)
-        Hlayout.addWidget(self.update_button)  # Güncelle butonunu ekleyelim
+        Hlayout.addWidget(self.update_button)  
         Hlayout.addWidget(self.delete_button)
 
-        # Hlayout'u bir QWidget'e sar
+        # Wrap Hlayout in a QWidget
         Hwidget = QtWidgets.QWidget()
         Hwidget.setLayout(Hlayout)
 
-        layout.addWidget(Hwidget)  # Sarılmış widget'ı ekleyin
+        layout.addWidget(Hwidget)  
         layout.addWidget(self.color_filter_box)
         layout.addWidget(self.record_table)
 
@@ -129,7 +129,7 @@ class CRUDApp(QtWidgets.QWidget):
         self.load_records()
 
     def enable_buttons(self):
-        # Seçili bir satır varsa sil ve güncelle butonlarını etkinleştir
+        # Enable delete and update buttons if a row is selected
         self.delete_button.setEnabled(self.record_table.currentRow() >= 0)
         self.update_button.setEnabled(self.record_table.currentRow() >= 0)
 
@@ -137,46 +137,46 @@ class CRUDApp(QtWidgets.QWidget):
         dialog = NewRecordDialog()
         if dialog.exec_() == QtWidgets.QDialog.Accepted:
             self.load_records()
-            self.show_message("Yeni kayıt başarıyla eklendi.")
+            self.show_message("New record added successfully.")
 
     def open_update_window(self):
         selected_row = self.record_table.currentRow()
         if selected_row >= 0:
-            # Seçilen satırın verilerini alarak UpdateWindow'a gönderelim
+            # Send the selected row's data to the UpdateWindow
             row_data = [self.record_table.item(selected_row, col).text() for col in range(6)]
             dialog = UpdateWindow(row_data)
             if dialog.exec_() == QtWidgets.QDialog.Accepted:
                 self.load_records()
-                self.show_message("Kayıt başarıyla güncellendi.")
+                self.show_message("Record updated successfully!")
 
     def delete_record(self):
-        selected_items = self.record_table.selectedItems()  # Seçilen öğeleri al
+        selected_items = self.record_table.selectedItems() 
     
         if not selected_items:
-            return  # Hiçbir satır seçilmemişse çık
+            return  
 
-        # Seçilen satırların indekslerini almak için bir set oluştur
+        # Create a set of selected row indices
         selected_rows = set(item.row() for item in selected_items)
 
-        # Renk numaralarını al
+        # Get the color numbers
         color_nos = [self.record_table.item(row, 1).text() for row in selected_rows]  # 1. sütundan renk numaralarını al
         
-        # Onay mesajı oluştur
+        # Confirmation message
         reply = QtWidgets.QMessageBox.question(
             self,
-            "Onayla",
-            f"{', '.join(color_nos)} renk kodlarına ait verileri silmek istediğinize emin misiniz?",
+            "Confirm",
+            f"{', '.join(color_nos)} Are you sure you want to delete data for the following color codes?",
             QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
             QtWidgets.QMessageBox.Yes
             )
 
         if reply == QtWidgets.QMessageBox.Yes:
-            for row in sorted(selected_rows, reverse=True):  # Ters sırayla sil
-                record_id = int(self.record_table.item(row, 0).text())  # 0. sütundan kayıt ID'sini al
-                delete_data(record_id)  # Her bir kayıt için silme işlemini yap
+            for row in sorted(selected_rows, reverse=True):  
+                record_id = int(self.record_table.item(row, 0).text()) 
+                delete_data(record_id) 
                 
-                self.load_records()  # Kayıtları tekrar yükle
-                self.show_message("Kayıtlar başarıyla silindi.")
+                self.load_records()  
+                self.show_message("Logs deleted successfully!")
 
 
 
@@ -205,7 +205,7 @@ class CRUDApp(QtWidgets.QWidget):
     def show_message(message):
         msg_box = QtWidgets.QMessageBox()
         msg_box.setText(message)
-        msg_box.setWindowTitle("Mesaj")
+        msg_box.setWindowTitle("Message")
         msg_box.exec_()
 
     def closeEvent(self, event):
@@ -217,20 +217,20 @@ class CRUDApp(QtWidgets.QWidget):
 class NewRecordDialog(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Kayıt Ekle")
+        self.setWindowTitle("Add Record")
         self.setGeometry(200, 200, 400, 300)
 
-        # Yeni kayıt bilgilerini gireceğimiz alanlar
-        self.color_no_label = QtWidgets.QLabel("Renk Numarası:")
+        # Fields for new record information
+        self.color_no_label = QtWidgets.QLabel("Color No:")
         self.color_no_entry = QtWidgets.QLineEdit()
-        self.cabinet_no_label = QtWidgets.QLabel("Dolap Numarası:")
+        self.cabinet_no_label = QtWidgets.QLabel("Cabinet No:")
         self.cabinet_no_entry = QtWidgets.QLineEdit()
-        self.cell_no_label = QtWidgets.QLabel("Hücre Numarası:")
+        self.cell_no_label = QtWidgets.QLabel("Cell No:")
         self.cell_no_entry = QtWidgets.QLineEdit()
-        self.lot_no_label = QtWidgets.QLabel("Lot Numarası:")
+        self.lot_no_label = QtWidgets.QLabel("Lot No:")
         self.lot_no_entry = QtWidgets.QLineEdit()
 
-        self.save_button = QtWidgets.QPushButton("Kaydet")
+        self.save_button = QtWidgets.QPushButton("Save")
         self.save_button.clicked.connect(self.save_record)
 
         # Layout
@@ -248,20 +248,19 @@ class NewRecordDialog(QtWidgets.QDialog):
 
     def save_record(self):
         try:
-            # Veri doğrulama
             color_no_text = self.color_no_entry.text()
             cabinet_no_text = self.cabinet_no_entry.text()
             cell_no_text = self.cell_no_entry.text()
             lot_no_text = self.lot_no_entry.text().strip()
 
             if not color_no_text:
-                raise ValueError("Renk Kodu boş bırakılmamalıdır!")
+                raise ValueError("Color number cannot be empty!")
             if not cabinet_no_text.isdigit():
-                raise ValueError("Dolap Numarası geçerli bir sayı olmalıdır!")
+                raise ValueError("Cabinet no cannot be empty!")
             if not cell_no_text.isdigit():
-                raise ValueError("Hücre Numarası geçerli bir sayı olmalıdır!")
+                raise ValueError("Cell no cannot be empty!")
             if lot_no_text and not lot_no_text.isdigit():
-                raise ValueError("Lot Numarası geçerli bir tam sayı olmalıdır!")
+                raise ValueError("Lot no cannot be empty!")
 
             insert_data(color_no_text, int(cabinet_no_text), int(cell_no_text), int(lot_no_text) if lot_no_text else None)
             self.accept()
@@ -269,11 +268,11 @@ class NewRecordDialog(QtWidgets.QDialog):
             CRUDApp.show_message(str(e))
             
 
-# Güncelleme penceresi
+# Updating Window
 class UpdateWindow(QtWidgets.QDialog):
     def __init__(self, record_data):
         super().__init__()
-        self.setWindowTitle("Kaydı Güncelle")
+        self.setWindowTitle("Update Record")
         self.setGeometry(200, 200, 400, 300)
 
         self.record_id = record_data[0]
@@ -283,17 +282,17 @@ class UpdateWindow(QtWidgets.QDialog):
         self.date_entry = QtWidgets.QLineEdit(record_data[4])
         self.lot_no_entry = QtWidgets.QLineEdit(record_data[5])
 
-        self.save_button = QtWidgets.QPushButton("Güncelle")
-        self.save_button.clicked.connect(self.save_update)  # `save_update` fonksiyonu doğru şekilde bağlı
+        self.save_button = QtWidgets.QPushButton("Update")
+        self.save_button.clicked.connect(self.save_update)  
 
         layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(QtWidgets.QLabel("Renk Numarası:"))
+        layout.addWidget(QtWidgets.QLabel("Color No:"))
         layout.addWidget(self.color_no_entry)
-        layout.addWidget(QtWidgets.QLabel("Dolap Numarası:"))
+        layout.addWidget(QtWidgets.QLabel("Cabinet No:"))
         layout.addWidget(self.cabinet_no_entry)
-        layout.addWidget(QtWidgets.QLabel("Hücre Numarası:"))
+        layout.addWidget(QtWidgets.QLabel("Cell No:"))
         layout.addWidget(self.cell_no_entry)
-        layout.addWidget(QtWidgets.QLabel("Lot Numarası:"))
+        layout.addWidget(QtWidgets.QLabel("Lot No:"))
         layout.addWidget(self.lot_no_entry)
         layout.addWidget(self.save_button)
 
@@ -302,7 +301,7 @@ class UpdateWindow(QtWidgets.QDialog):
     def save_update(self):
         try:
             color_no_text = self.color_no_entry.text()
-            cabinet_no_text = self.cabinet_no_entry.text()  # Eğer None veya boşsa 0 yap
+            cabinet_no_text = self.cabinet_no_entry.text() 
             cell_no_text = self.cell_no_entry.text()
             #lot_no_text = self.lot_no_entry.text() or ""
             #lot_no_text = self.lot_no_entry.text() if self.lot_no_entry.text() != 'None' None else None
@@ -313,19 +312,18 @@ class UpdateWindow(QtWidgets.QDialog):
             else:
                 lot_no_text=None
             if not color_no_text:
-                raise ValueError("Renk Kodu boş bırakılmamalıdır!")
+                raise ValueError("Color number cannot be empty!")
             if not cabinet_no_text.isdigit():
-                raise ValueError("Dolap Numarası geçerli bir sayı olmalıdır!")
+                raise ValueError("Cabinet number cannot be empty!")
             if not cell_no_text.isdigit():
-                raise ValueError("Hücre Numarası geçerli bir sayı olmalıdır!")
+                raise ValueError("Cell number cannot be empty!")
             if lot_no_text and not lot_no_text.isdigit():
-                raise ValueError("Lot Numarası geçerli bir tam sayı olmalıdır!")
+                raise ValueError("Lot number cannot be empty!")
 
 
-            # Tarih için mevcut zamanı alalım
-            date_text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Mevcut tarihi ve saati al
+            # Get the updated data
+            date_text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  
 
-            # Güncelleme fonksiyonuna gönderelim
             update_data(int(self.record_id), color_no_text, int(cabinet_no_text), int(cell_no_text), lot_no_text, date_text)
             self.accept()
         except Exception as e:
